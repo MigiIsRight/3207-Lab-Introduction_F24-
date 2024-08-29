35cf30250b1f8e9a8394f729875c4d05578960e3
Jared McPherson <tup30869@temple.edu>
2024-08-29 11:14:31 -0400

    Added a program random.c to create random letters

---
+++ random.c
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{   
+    
+
+    
+
+    return 0;
+}
+
+char randchar(){
+    srand(time(NULL));
+    
+    char h[25]= {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','z'};
+    
+    int x=(rand()%26) ;
+    return h[x];
+}
