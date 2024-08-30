4894b4d66984e23e8cd6f58c27f0920cb051e7ca
Jared McPherson <tup30869@temple.edu>
2024-08-30 15:17:55 -0400

    add random.c and random.h files

--- random.c
+++ random.c
@@ -9,19 +9,14 @@ Welcome to GDB Online.
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{   
-    
 
-    
-
-    return 0;
-}
 
 char randchar(){
-    srand(time(NULL));
     
-    char h[25]= {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','z'};
+    
+    char h[25]= {'a','b','c','d','e','f','g','h','i',
+    'j','k','l','m','n','o','p',
+    'q','r','s','t','u','v','w','x','z'};
     
     int x=(rand()%26) ;
     return h[x];
